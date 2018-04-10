1b246f47ce73d47713570ae384ccc2ee491f7562
Aaron Moore <x2kicker2x@gmail.com>
2018-04-10 16:52:05 -0400

    Got this

--- definitions/sliding-entrances.js
+++ definitions/sliding-entrances.js
@@ -1,7 +1,7 @@
 function makeSlideInTranslation(translationType, fromValue) {
   return {
     from: {
-      [translationType]: 0,
+      [translationType]: fromValue,
     },
     to: {
       [translationType]: 0,
@@ -9,7 +9,7 @@ function makeSlideInTranslation(translationType, fromValue) {
   };
 }
 
-export const slideInDown = makeSlideInTranslation('translateY', -50);
+export const slideInDown = makeSlideInTranslation('translateY', -20);
 
 export const slideInUp = makeSlideInTranslation('translateY', 100);
 
